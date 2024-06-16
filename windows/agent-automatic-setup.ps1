fda7a94ac146edeabaf37d28665c3c44373e0810
jonmacphers <jonmacphers@gmail.com>
2024-06-16 08:58:35 -0400

    added remove-threat script and powershell setup

--- windows/agent-automatic-setup.ps1
+++ windows/agent-automatic-setup.ps1
@@ -16,16 +16,46 @@ Set-Content -Path $configPath -Value $config
 # File Integrity Monitoring Configuration
 $ossecConfPath = $configPath
 $newDirectory = @"
-  <directories check_all="yes" realtime="yes">%USERPROFILE%\Downloads</directories>
+<directories check_all="yes" realtime="yes">$env:USERPROFILE\Downloads</directories>
 "@
 [xml]$ossecConf = Get-Content -Path $ossecConfPath
 $syscheckNode = $ossecConf.ossec_config.syscheck
 if (-not $syscheckNode) {
     $syscheckNode = $ossecConf.CreateElement("syscheck")
-    $ossecConf.ossec_config.AppendChild($syscheckNode)
+    $ossecConf.ossec_config.AppendChild($syscheckNode) | Out-Null
 }
-$syscheckNode.InnerXml += $newDirectory
+
+# Find the comment and add the new directory after it
+$commentNode = $ossecConf.ossec_config.syscheck.SelectSingleNode("comment()[contains(.,'<!-- Default files to be monitored. -->')]")
+if ($commentNode) {
+    $newDirectoryNode = $ossecConf.CreateElement("directories")
+    $newDirectoryNode.SetAttribute("check_all", "yes")
+    $newDirectoryNode.SetAttribute("realtime", "yes")
+    $newDirectoryNode.InnerText = "$env:USERPROFILE\Downloads"
+    $syscheckNode.InsertAfter($newDirectoryNode, $commentNode) | Out-Null
+} else {
+    $fragment = $ossecConf.CreateDocumentFragment()
+    $fragment.InnerXml = $newDirectory
+    $syscheckNode.AppendChild($fragment) | Out-Null
+}
+
 $ossecConf.Save($ossecConfPath)
+
+# Install Python and PyInstaller
+Start-Process -FilePath "python-installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -Wait
+Start-Process -FilePath "pip" -ArgumentList "install pyinstaller" -Wait
+
+# Define the URL of the Git repository
+$gitRepoUrl = "https://github.com/thenexlabs/nixguard-droplet-scripts/raw/main/wazuh-agent-automation/windows/remove-threat.py"
+
+# Clone the Git repository
+Start-Process -FilePath "git" -ArgumentList "clone $gitRepoUrl" -Wait
+
+# Define the path to the remove-threat.py script in the cloned repository
+$removeThreatPath = Join-Path -Path (Join-Path -Path $PWD.Path -ChildPath (Split-Path -Path $gitRepoUrl -Leaf)) -ChildPath "remove-threat.py"
+
+# Convert the remove-threat.py script to a Windows executable
+Start-Process -FilePath "pyinstaller" -ArgumentList "-F $removeThreatPath" -Wait
 
 Write-Host "Directory monitoring configuration added successfully."
 
