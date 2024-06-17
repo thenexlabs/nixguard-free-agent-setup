f0df35b2991fabea1671ce592a9a2d00c89bd0e1
jonmacphers <jonmacphers@gmail.com>
2024-06-17 12:42:31 -0400

    added remove-threat script and powershell setup & linux equivalents

--- linux/agent-automatic-setup.sh
+++ linux/agent-automatic-setup.sh
@@ -47,6 +47,60 @@ install_wazuh_agent() {
             wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.3-1_arm64.deb
             sudo WAZUH_MANAGER="$WAZUH_MANAGER" WAZUH_AGENT_NAME="$WAZUH_AGENT_NAME" dpkg -i ./wazuh-agent_4.7.3-1_arm64.deb
         fi
+            # Define the path to the OSSEC configuration file
+            ossecConfPath="/var/ossec/etc/ossec.conf"
+
+            # Set the manager IP in the ossec.conf file
+            sed -i "s/<address>.*<\/address>/<address>${MANAGER_IP}<\/address>/g" $ossecConfPath
+
+            # Define the new directory to monitor
+            newDirectory="<directories check_all=\"yes\" realtime=\"yes\">/root</directories>"
+
+            # Check if the syscheck section exists
+            if ! grep -q "<syscheck>" $ossecConfPath; then
+                # If syscheck section does not exist, create it
+                sed -i '/<\/ossec_config>/i \ \ <syscheck>\n\ \ </syscheck>' $ossecConfPath
+            fi
+
+            # Add the new directory monitoring configuration
+            sed -i "/<syscheck>/a \ \ $newDirectory" $ossecConfPath
+
+            echo "Directory monitoring configuration added successfully."
+
+            ###########################################################################################
+
+            # Set the current directory to the user's home directory
+            # cd ~
+
+            sudo apt update
+            sudo apt -y install jq
+
+            # Define the URL of the remove-threat.py script
+            removeThreatUrl="https://github.com/thenexlabs/nixguard-agent-setup/raw/main/linux/remove-threat.sh"
+
+            # Define the path to save the remove-threat.sh script
+            removeThreatPath="/remove-threat.sh"
+
+            # Download the remove-threat.py script
+            curl -o $removeThreatPath $removeThreatUrl
+
+            # Define the path of the executable file
+            exePath="$(pwd)/remove-threat.sh"
+
+            # Define the destination directory
+            destDir="/var/ossec/active-response/bin"
+
+            # Move the executable file to the destination directory
+            sudo mv $exePath $destDir
+
+            # Clean up the build artifacts
+            # rm -rf /tmp/remove-threat.py /tmp/dist /tmp/build /tmp/remove-threat.spec
+
+            echo "Virus threat response configuration added successfully."
+
+            ###########################################################################################
+
+            echo "NixGuard agent setup successfully."
     elif [ "$distro" == "centos" ] || [ "$distro" == "rhel" ] || [ "$distro" == "fedora" ]; then
         if [ "$arch" == "amd64" ]; then
             curl -o wazuh-agent-4.7.3-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.3-1.x86_64.rpm
@@ -64,6 +118,8 @@ install_wazuh_agent() {
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
+
+    echo "NixGuard agent started successfully."
 }
 
 # Main script execution
@@ -77,27 +133,3 @@ WAZUH_AGENT_NAME="$AGENT_NAME"
 
 detect_distro_arch
 install_wazuh_agent "$WAZUH_MANAGER" "$WAZUH_AGENT_NAME"
-
-# Define the path to the OSSEC configuration file
-ossecConfPath="/var/ossec/etc/ossec.conf"
-
-# Set the manager IP in the ossec.conf file
-sed -i "s/<address>.*<\/address>/<address>${MANAGER_IP}<\/address>/g" $ossecConfPath
-
-# Define the new directory to monitor
-newDirectory="<directories check_all="yes" realtime=\"yes\">/media/user/software</directories>"
-
-# Check if the syscheck section exists
-if ! grep -q "<syscheck>" $ossecConfPath; then
-    # If syscheck section does not exist, create it
-    sed -i '/<\/ossec_config>/i \ \ <syscheck>\n\ \ </syscheck>' $ossecConfPath
-fi
-
-# Add the new directory monitoring configuration
-sed -i "/<syscheck>/a \ \ $newDirectory" $ossecConfPath
-
-echo "Directory monitoring configuration added successfully."
-
-
-# Start the Wazuh service
-sudo systemctl start wazuh-agent
--- windows/agent-automatic-setup.ps1
+++ windows/agent-automatic-setup.ps1
@@ -3,12 +3,87 @@ param (
   [string]$ipAddress
 )
 
+function Uninstall-WazuhAgent {
+    # Stop the Wazuh service
+    Stop-Service -Name "WazuhSvc" -ErrorAction SilentlyContinue
+
+    # Uninstall the Wazuh agent using msiexec
+    msiexec.exe /x $env:tmp\wazuh-agent.msi /q 2>$null
+
+    # Remove the Wazuh agent installation directory
+    Remove-Item -Recurse -Force "C:\Program Files (x86)\ossec-agent" -ErrorAction SilentlyContinue
+
+    # Remove the Wazuh agent installation directory
+    Remove-Item -Recurse -Force "C:\wazuh-agent" -ErrorAction SilentlyContinue
+}
+
+# run Uninstall-WazuhAgent function
+Uninstall-WazuhAgent
+
 # Install the Wazuh agent
-$wazuhInstaller = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "${env:tmp}\wazuh-agent", "/q", "WAZUH_MANAGER=$ipAddress", "WAZUH_AGENT_NAME=$agentName", "WAZUH_REGISTRATION_SERVER=$ipAddress" -PassThru
-$wazuhInstaller.WaitForExit()
+$configPath = 'C:\Program Files (x86)\ossec-agent\ossec.conf'
+
+# loop until file is gone
+$fileExists = $true
+while ($fileExists) {
+    # Check if the file exists
+    if (Test-Path -Path $configPath) {
+        # If the file exists, print the message and wait for a while before checking again
+        # Write-Host "File found at $configPath, waiting for 5 seconds before checking again..."
+        Write-Host ".."
+        Start-Sleep -Seconds 3
+    } else {
+        # If the file does not exist, set the control variable to false to exit the loop
+        $fileExists = $false
+        # Write-Host "File not found at $configPath"
+        Write-Host "."
+    }
+}
+
+# Set a maximum number of retries
+$maxRetries = 5
+$retryCount = 0
+
+# Start the installation process
+do {
+    $wazuhInstaller = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", "${env:tmp}\wazuh-agent", "/q", "WAZUH_MANAGER=$ipAddress", "WAZUH_AGENT_NAME=$agentName", "WAZUH_REGISTRATION_SERVER=$ipAddress" -PassThru
+    $wazuhInstaller.WaitForExit()
+
+    # Check the exit code of the installer
+    if ($wazuhInstaller.ExitCode -ne 0) {
+        Write-Host "Installer exited with code $($wazuhInstaller.ExitCode)"
+        $retryCount++
+        if ($retryCount -le $maxRetries) {
+            Write-Host "Retrying installation ($retryCount of $maxRetries)..."
+            Start-Sleep -Seconds 10  # Wait before retrying
+        } else {
+            Write-Host "Installation failed after $maxRetries attempts."
+            exit $wazuhInstaller.ExitCode
+        }
+    } else {
+        # If the exit code is 0, break the loop
+        break
+    }
+} while ($true)
+
+# loop until file exists
+$counter = 0
+$fileExists = $false
+while (-not $fileExists) {
+    # Check if the file exists
+    if (Test-Path -Path $configPath) {
+        # If the file exists, set the control variable to true to exit the loop
+        $fileExists = $true
+        Write-Host "."
+    } else {
+        # If the file does not exist, wait for a while before checking again
+        Write-Host ".."
+        Start-Sleep -Seconds 3
+    }
+    $counter++
+}
 
 # Update the ossec.conf file
-$configPath = 'C:\Program Files (x86)\ossec-agent\ossec.conf'
 $config = Get-Content -Path $configPath
 $config = $config -replace '<address>0.0.0.0</address>', "<address>$ipAddress</address>"
 Set-Content -Path $configPath -Value $config
@@ -16,10 +91,17 @@ Set-Content -Path $configPath -Value $config
 # File Integrity Monitoring Configuration
 $ossecConfPath = $configPath
 $newDirectory = @"
-<directories check_all="yes" realtime="yes">$env:USERPROFILE\Downloads</directories>
+<directories check_all="yes" whodata="yes" realtime="yes">$env:USERPROFILE\Downloads</directories>
 "@
 [xml]$ossecConf = Get-Content -Path $ossecConfPath
+
+# Wait until the file is found
+while (-not $ossecConf) {
+    Start-Sleep -Seconds 1
+}
+
 $syscheckNode = $ossecConf.ossec_config.syscheck
+
 if (-not $syscheckNode) {
     $syscheckNode = $ossecConf.CreateElement("syscheck")
     $ossecConf.ossec_config.AppendChild($syscheckNode) | Out-Null
@@ -30,6 +112,7 @@ $commentNode = $ossecConf.ossec_config.syscheck.SelectSingleNode("comment()[cont
 if ($commentNode) {
     $newDirectoryNode = $ossecConf.CreateElement("directories")
     $newDirectoryNode.SetAttribute("check_all", "yes")
+    $newDirectoryNode.SetAttribute("whodata", "yes")
     $newDirectoryNode.SetAttribute("realtime", "yes")
     $newDirectoryNode.InnerText = "$env:USERPROFILE\Downloads"
     $syscheckNode.InsertAfter($newDirectoryNode, $commentNode) | Out-Null
@@ -41,23 +124,66 @@ if ($commentNode) {
 
 $ossecConf.Save($ossecConfPath)
 
-# Install Python and PyInstaller
-Start-Process -FilePath "python-installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -Wait
-Start-Process -FilePath "pip" -ArgumentList "install pyinstaller" -Wait
-
-# Define the URL of the Git repository
-$gitRepoUrl = "https://github.com/thenexlabs/nixguard-droplet-scripts/raw/main/wazuh-agent-automation/windows/remove-threat.py"
-
-# Clone the Git repository
-Start-Process -FilePath "git" -ArgumentList "clone $gitRepoUrl" -Wait
-
-# Define the path to the remove-threat.py script in the cloned repository
-$removeThreatPath = Join-Path -Path (Join-Path -Path $PWD.Path -ChildPath (Split-Path -Path $gitRepoUrl -Leaf)) -ChildPath "remove-threat.py"
-
-# Convert the remove-threat.py script to a Windows executable
-Start-Process -FilePath "pyinstaller" -ArgumentList "-F $removeThreatPath" -Wait
-
 Write-Host "Directory monitoring configuration added successfully."
 
+###########################################################################################
+
+# Set the current directory to the user's home directory
+Set-Location -Path $env:USERPROFILE
+
+# Define the URL of the Python installer
+$pythonUrl = "https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe"
+
+# Define the path to save the Python installer
+$pythonInstallerPath = Join-Path -Path $env:TEMP -ChildPath "python-installer.exe"
+
+# Download the Python installer
+Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonInstallerPath
+
+# Run the Python installer
+Start-Process -FilePath $pythonInstallerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -Wait
+
+# Start a new PowerShell session to install PyInstaller
+Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -Command {py -m pip install pyinstaller}" -Wait
+
+# Define the URL of the remove-threat.py script
+$removeThreatUrl = "https://github.com/thenexlabs/nixguard-agent-setup/raw/main/windows/remove-threat.py"
+
+# Define the path to save the remove-threat.py script
+$removeThreatPath = Join-Path -Path $env:TEMP -ChildPath "remove-threat.py"
+
+# Download the remove-threat.py script
+Invoke-WebRequest -Uri $removeThreatUrl -OutFile $removeThreatPath
+
+# Convert the remove-threat.py script to a Windows executable
+Invoke-Expression -Command "py -m PyInstaller -F $removeThreatPath"
+
+# Define the path of the executable file
+$exePath = Join-Path -Path (Get-Location) -ChildPath "dist\remove-threat.exe"
+
+# Define the destination directory
+$destDir = "C:\Program Files (x86)\ossec-agent\active-response\bin"
+
+# Move the executable file to the destination directory
+Move-Item -Path $exePath -Destination $destDir
+
+# Define the paths of the spec file and the dist and build directories
+$specPath = Join-Path -Path (Get-Location) -ChildPath "remove-threat.spec"
+$distDir = Join-Path -Path (Get-Location) -ChildPath "dist"
+$buildDir = Join-Path -Path (Get-Location) -ChildPath "build"
+
+# Delete the spec file and the dist and build directories
+Remove-Item -Path $specPath -ErrorAction SilentlyContinue
+Remove-Item -Path $distDir -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path $buildDir -Recurse -ErrorAction SilentlyContinue
+
+Write-Host "Virus threat response configuration added successfully."
+
+###########################################################################################
+
+Write-Host "NixGuard agent setup successfully."
+
 # Start Wazuh agent
-NET START WazuhSvc
+Start-Process -FilePath "NET" -ArgumentList "START WazuhSvc"
+
+Write-Host "NixGuard agent started successfully."
