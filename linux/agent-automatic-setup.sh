badb58751c5a690aa9be5bbcc9aff9bf0f1abb1c
Jonathan Catalano <Jonmacphers@gmail.com>
2024-11-14 22:50:07 -0500

    Update agent-automatic-setup.sh

--- linux/agent-automatic-setup.sh
+++ linux/agent-automatic-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # ./scriptname.sh "your_manager_ip" "your_agent_name"
 
-
 # Check if two arguments are passed
 if [ "$#" -ne 2 ]; then
     echo "Usage: ./scriptname.sh <manager_ip> <agent_name>"
@@ -58,8 +57,8 @@ uninstall_wazuh_agent() {
 
 # Function to fix broken dependencies
 fix_dependencies() {
-    sudo apt-get update
-    sudo apt-get -f install -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -f install -y
 }
 
 # Function to install Wazuh agent
@@ -68,93 +67,86 @@ install_wazuh_agent() {
     local WAZUH_AGENT_NAME="$AGENT_NAME"
     local WAZUH_AGENT_GROUP="default"  # Adding the missing agent group
 
-    echo "private cloud soc ip: $WAZUH_MANAGER"
-    echo "agent name: $WAZUH_AGENT_NAME"
-    echo "agent group: $WAZUH_AGENT_GROUP"
+    echo "Private cloud SOC IP: $WAZUH_MANAGER"
+    echo "Agent name: $WAZUH_AGENT_NAME"
+    echo "Agent group: $WAZUH_AGENT_GROUP"
 
     if [ "$distro" == "debian" ] || [ "$distro" == "ubuntu" ] || [ "$distro" == "kali" ]; then
         if [ "$arch" == "amd64" ]; then
             wget -O wazuh-agent_nixguard_amd64.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.9.1-1_amd64.deb
-            sudo WAZUH_MANAGER="$WAZUH_MANAGER" WAZUH_AGENT_NAME="$WAZUH_AGENT_NAME" WAZUH_AGENT_GROUP="$WAZUH_AGENT_GROUP" dpkg -i ./wazuh-agent_nixguard_amd64.deb
+            sudo DEBIAN_FRONTEND=noninteractive dpkg -i ./wazuh-agent_nixguard_amd64.deb
         elif [ "$arch" == "aarch64" ]; then
             wget -O wazuh-agent_nixguard_arm64.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.9.1-1_arm64.deb
-            sudo WAZUH_MANAGER="$WAZUH_MANAGER" WAZUH_AGENT_NAME="$WAZUH_AGENT_NAME" WAZUH_AGENT_GROUP="$WAZUH_AGENT_GROUP" dpkg -i ./wazuh-agent_nixguard_arm64.deb
+            sudo DEBIAN_FRONTEND=noninteractive dpkg -i ./wazuh-agent_nixguard_arm64.deb
         fi
-            # Define the path to the OSSEC configuration file
-            ossecConfPath="/var/ossec/etc/ossec.conf"
 
-            # Set the manager IP in the ossec.conf file
-            sudo sed -i "s/<address>.*<\/address>/<address>${WAZUH_MANAGER}<\/address>/g" $ossecConfPath
+        # Fix dependencies
+        fix_dependencies
 
-            # Define the enrollment section
-            ENROLLMENT_SECTION="<enrollment>\n\t<enabled>yes</enabled>\n\t<manager_address>${WAZUH_MANAGER}</manager_address>\n\t<agent_name>${WAZUH_AGENT_NAME}</agent_name>\n</enrollment>"
+        # Define the path to the OSSEC configuration file
+        ossecConfPath="/var/ossec/etc/ossec.conf"
 
-            # Add the enrollment section to the ossec.conf file
-            sudo awk -v enrollment="$ENROLLMENT_SECTION" '
-                /<client>/ { print; print enrollment; next }
-                !/<enrollment>/ { print }
-            ' "$ossecConfPath" > temp_ossec.conf && sudo mv temp_ossec.conf "$ossecConfPath"
+        # Set the manager IP in the ossec.conf file
+        sudo sed -i "s/<address>.*<\/address>/<address>${WAZUH_MANAGER}<\/address>/g" $ossecConfPath
 
-            # Define the new directories to monitor
-            directories=(
-                "<directories check_all=\"yes\" realtime=\"yes\">/root</directories>"  # Root directory
-                "<directories check_all=\"yes\" realtime=\"yes\">/etc</directories>"  # Configuration files
-                "<directories check_all=\"yes\" realtime=\"yes\">/var</directories>"  # Variable files (limited)
-                "<directories check_all=\"yes\" realtime=\"yes\">/usr</directories>"  # User programs
-                "<directories check_all=\"yes\" realtime=\"yes\">/home</directories>"  # Home directories
-                # "<directories check_all=\"yes\" realtime=\"yes\">/boot</directories>"  # Boot files
-                # "<directories check_all=\"yes\" realtime=\"yes\">/opt</directories>"  # Optional software
-                # "<directories check_all=\"yes\" realtime=\"yes\">/sbin</directories>"  # System binaries
-                "<directories check_all=\"yes\" realtime=\"yes\">/bin</directories>"  # Binaries
-                # "<directories check_all=\"yes\" realtime=\"yes\">/lib</directories>"  # Libraries
-                # "<directories check_all=\"yes\" realtime=\"yes\">/lib64</directories>"  # 64-bit libraries
-            )
-            # Excluding the /tmp directory as it typically contains many transient files
+        # Define the enrollment section
+        ENROLLMENT_SECTION="<enrollment>\n\t<enabled>yes</enabled>\n\t<manager_address>${WAZUH_MANAGER}</manager_address>\n\t<agent_name>${WAZUH_AGENT_NAME}</agent_name>\n</enrollment>"
 
-            # Check if the syscheck section exists
-            if ! sudo grep -q "<syscheck>" $ossecConfPath; then
-                # If syscheck section does not exist, create it
-                sudo sed -i '/<\/ossec_config>/i \ \ <syscheck>\n\ \ </syscheck>' $ossecConfPath
-            fi
+        # Add the enrollment section to the ossec.conf file
+        sudo awk -v enrollment="$ENROLLMENT_SECTION" '
+            /<client>/ { print; print enrollment; next }
+            !/<enrollment>/ { print }
+        ' "$ossecConfPath" > temp_ossec.conf && sudo mv temp_ossec.conf "$ossecConfPath"
 
-            # Add the new directory monitoring configuration
-            for directory in "${directories[@]}"; do
-                sudo sed -i "/<syscheck>/a \ \ $directory" $ossecConfPath
-            done
+        # Define the new directories to monitor
+        directories=(
+            "<directories check_all=\"yes\" realtime=\"yes\">/root</directories>"  # Root directory
+            "<directories check_all=\"yes\" realtime=\"yes\">/etc</directories>"  # Configuration files
+            "<directories check_all=\"yes\" realtime=\"yes\">/var</directories>"  # Variable files (limited)
+            "<directories check_all=\"yes\" realtime=\"yes\">/usr</directories>"  # User programs
+            "<directories check_all=\"yes\" realtime=\"yes\">/home</directories>"  # Home directories
+            "<directories check_all=\"yes\" realtime=\"yes\">/bin</directories>"  # Binaries
+        )
+        # Excluding the /tmp directory as it typically contains many transient files
 
-            echo "Directory monitoring configuration added successfully."
+        # Check if the syscheck section exists
+        if ! sudo grep -q "<syscheck>" $ossecConfPath; then
+            # If syscheck section does not exist, create it
+            sudo sed -i '/<\/ossec_config>/i \ \ <syscheck>\n\ \ </syscheck>' $ossecConfPath
+        fi
 
-            ###########################################################################################
+        # Add the new directory monitoring configuration
+        for directory in "${directories[@]}"; do
+            sudo sed -i "/<syscheck>/a \ \ $directory" $ossecConfPath
+        done
 
-            # Set the current directory to the user's home directory
-            # cd ~
+        echo "Directory monitoring configuration added successfully."
 
-            sudo apt update
-            sudo apt -y install jq
+        ###########################################################################################
 
-            # Define the URL of the remove-threat.sh script
-            removeThreatUrl="https://github.com/thenexlabs/nixguard-agent-setup/raw/main/linux/remove-threat.sh"
+        sudo apt update
+        sudo apt -y install jq
 
-            # Define the destination directory
-            destDir="/var/ossec/active-response/bin"
+        # Define the URL of the remove-threat.sh script
+        removeThreatUrl="https://github.com/thenexlabs/nixguard-agent-setup/raw/main/linux/remove-threat.sh"
 
-            # Define the path to save the remove-threat.sh script in the destination directory
-            removeThreatPath="$destDir/remove-threat.sh"
+        # Define the destination directory
+        destDir="/var/ossec/active-response/bin"
 
-            # Download the remove-threat.sh script
-            sudo curl -o $removeThreatPath $removeThreatUrl
+        # Define the path to save the remove-threat.sh script in the destination directory
+        removeThreatPath="$destDir/remove-threat.sh"
 
-            sudo chmod 750 /var/ossec/active-response/bin/remove-threat.sh
-            sudo chown root:wazuh /var/ossec/active-response/bin/remove-threat.sh
+        # Download the remove-threat.sh script
+        sudo curl -o $removeThreatPath $removeThreatUrl
 
-            # Clean up the build artifacts
-            # rm -rf /tmp/remove-threat.py /tmp/dist /tmp/build /tmp/remove-threat.spec
+        sudo chmod 750 /var/ossec/active-response/bin/remove-threat.sh
+        sudo chown root:wazuh /var/ossec/active-response/bin/remove-threat.sh
 
-            echo "Virus threat response configuration added successfully."
+        echo "Virus threat response configuration added successfully."
 
-            ###########################################################################################
+        ###########################################################################################
 
-            echo "NixGuard agent setup successfully."
+        echo "NixGuard agent setup successfully."
     elif [ "$distro" == "centos" ] || [ "$distro" == "rhel" ] || [ "$distro" == "fedora" ]; then
         if [ "$arch" == "amd64" ]; then
             curl -O wazuh-agent_nixguard.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.9.1-1.x86_64.rpm
@@ -185,10 +177,7 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-
-# function calls
+# Function calls
 detect_distro_arch
-
 uninstall_wazuh_agent
-
 install_wazuh_agent
